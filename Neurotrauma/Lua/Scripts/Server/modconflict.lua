142e7249c2a92fdb4599dab258a51b0e27772680
OlegBSTU <polybox45@gmail.com>
2024-04-22 21:48:27 +0300

    neurotrauma patches never trigger modconflict
    fixed an issue where using a patch would show up modconflict affliction and console message about the mod conflict

--- Neurotrauma/Lua/Scripts/Server/modconflict.lua
+++ Neurotrauma/Lua/Scripts/Server/modconflict.lua
@@ -1,3 +1,7 @@
+-- Hooks a Lua event "roundStart" to use for applying Neurotrauma specific
+-- meta affliction named "modconflict", then console message about the name
+-- of incompatible mod. Done also on server startup. 
+-- It's important for patch to have the word "neurotrauma" in their name (case doesnt matter)
 NT.modconflict = false
 function NT.CheckModConflicts()
     NT.modconflict = false
@@ -8,7 +12,7 @@ function NT.CheckModConflicts()
     for prefab in ItemPrefab.Prefabs do
         if HF.TableContains(itemsToCheck,prefab.Identifier.Value) then
             local mod = prefab.ConfigElement.ContentPackage.Name
-            if mod ~= NT.Name then
+            if not string.find(string.lower(mod), "neurotrauma") then
                 NT.modconflict = true
 				print("Found Neurotrauma incompatibility with mod: ", mod)
                 print("WARNING! mod conflict detected! Neurotrauma may not function correctly and requires a patch!")
