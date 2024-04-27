8da30d83d5cdeb9e158f2336a1ce8d248df82b8d
OlegBSTU <polybox45@gmail.com>
2024-04-28 01:08:46 +0300

    Balanced ointment item by Paws

--- Neurotrauma/Lua/Autorun/init.lua
+++ Neurotrauma/Lua/Autorun/init.lua
@@ -2,8 +2,8 @@
 
 NT = {} -- Neurotrauma
 NT.Name="Neurotrauma"
-NT.Version = "A1.9.0"
-NT.VersionNum = 01090000
+NT.Version = "A1.9.1"
+NT.VersionNum = 01090001
 NT.Path = table.pack(...)[1]
 
 dofile(NT.Path.."/Lua/Scripts/helperfunctions.lua")
