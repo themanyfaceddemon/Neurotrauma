d9ad1777a47016c635cf7dee960ac3758c2fef8e
OlegBSTU <polybox45@gmail.com>
2024-06-01 12:24:04 +0300

    Version change

--- Neurotrauma/Lua/Autorun/init.lua
+++ Neurotrauma/Lua/Autorun/init.lua
@@ -2,8 +2,8 @@
 
 NT = {} -- Neurotrauma
 NT.Name="Neurotrauma"
-NT.Version = "A1.9.2h3"
-NT.VersionNum = 01090202
+NT.Version = "A1.9.2h4"
+NT.VersionNum = 01090204
 NT.Path = table.pack(...)[1]
 
 dofile(NT.Path.."/Lua/Scripts/helperfunctions.lua")
