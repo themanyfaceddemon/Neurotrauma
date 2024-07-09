789024e5377123055525903a6156e41044951b73
OlegBSTU <polybox45@gmail.com>
2024-07-09 23:25:01 +0300

    Fresh limbs sub transfer, base adrenaline sprite

--- Neurotrauma/Lua/Autorun/init.lua
+++ Neurotrauma/Lua/Autorun/init.lua
@@ -2,8 +2,8 @@
 
 NT = {} -- Neurotrauma
 NT.Name="Neurotrauma"
-NT.Version = "A1.9.4"
-NT.VersionNum = 01090400
+NT.Version = "A1.9.4h1"
+NT.VersionNum = 01090401
 NT.Path = table.pack(...)[1]
 
 dofile(NT.Path.."/Lua/Scripts/helperfunctions.lua")
