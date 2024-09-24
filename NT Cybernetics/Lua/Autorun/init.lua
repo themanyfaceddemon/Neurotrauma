b6b865d27ef042c2ef981ffa885e21c5a61105b1
Nebual <ben1120@gmail.com>
2024-09-24 01:14:53 -0700

    Add chat warning for using old NT

--- NT Cybernetics/Lua/Autorun/init.lua
+++ NT Cybernetics/Lua/Autorun/init.lua
@@ -3,8 +3,8 @@ NTCyb = {} -- Neurotrauma Cybernetics
 NTCyb.Name="Cybernetics"
 NTCyb.Version = "A1.3.0"
 NTCyb.VersionNum = 01030000
-NTCyb.MinNTVersion = "A1.8.7"
-NTCyb.MinNTVersionNum = 01080700
+NTCyb.MinNTVersion = "A1.9.0"
+NTCyb.MinNTVersionNum = 01090000
 NTCyb.Path = table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTCyb) end end,1)
 
@@ -12,8 +12,14 @@ Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.Re
 if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
 
     Timer.Wait(function()
+        if NT ~= nil and NT.VersionNum < 01090000 then
+            print("Error loading NT Cybernetics: old Neurotrauma detected, use the modern fork published by 'guns'")
+            Game.SendMessage("Error loading NT Cybernetics: old Neurotrauma detected, use the modern fork published by 'guns'")
+            return
+        end
         if NTC == nil then
             print("Error loading NT Cybernetics: It appears Neurotrauma isn't loaded!")
+            Game.SendMessage("Error loading NT Cybernetics: It appears Neurotrauma isn't loaded!", ChatMessageType.Server)
             return
         end
 
@@ -31,6 +37,12 @@ if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
     end,1)
 else
     Timer.Wait(function()
+        if NT ~= nil and NT.VersionNum < 01090000 then
+            local msg = "Error loading NT Cybernetics: old Neurotrauma detected, use the modern fork published by 'guns'"
+            print(msg)
+            Game.ChatBox.AddMessage(ChatMessage.Create("", msg, ChatMessageType.Server, nil))
+            return
+        end
         dofile(NTCyb.Path.."/Lua/Scripts/items.client.lua")
         dofile(NTCyb.Path.."/Lua/Scripts/items.shared.lua")
     end, 1)
